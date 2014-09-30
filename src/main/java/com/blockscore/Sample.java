e9ce9c815a9a7c6e0ecdbe4e92a888f46e3ae50c
Tony Dieppa <tony@viralloop.co>
2014-09-29 19:42:00 -0700

    Started work on the models

--- src/main/java/com/blockscore/Sample.java
+++ src/main/java/com/blockscore/Sample.java
@@ -1,7 +1,16 @@
 package com.blockscore;
 
+import com.blockscore.net.BlockscoreApiClient;
+
 /**
+ * Example class on how to use the Blockscore API client.
  * Created by tealocean on 9/29/14.
  */
 public class Sample {
+    public static void main(final String[] args) {
+        BlockscoreApiClient.init("sk_test_3380b53cc2ae5b78910344c49f334c2e");
+        BlockscoreApiClient.useVerboseLogs(true);
+        BlockscoreApiClient apiClient = new BlockscoreApiClient();
+        apiClient.createVerification();
+    }
 }
--- src/main/java/com/blockscore/models/Person.java
+++ src/main/java/com/blockscore/models/Person.java
@@ -1,7 +1,20 @@
 package com.blockscore.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Date;
+
 /**
- * Created by tealocean on 9/29/14.
+ * Model representing a person's identity.
+ * Created by Tony Dieppa on 9/29/14.
  */
 public class Person {
+    @NotNull
+    @JsonProperty("date_of_birth")
+    private Date mDateOfBirth;
+
+    @NotNull
+    @JsonProperty("identification")
+    private Identification mIdentification;
 }
