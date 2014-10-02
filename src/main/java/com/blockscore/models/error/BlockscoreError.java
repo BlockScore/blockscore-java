17f4d56f4481039ee1d8bf4d1ff6588a1fa01e38
Tony Dieppa <tony@viralloop.co>
2014-10-01 23:20:10 -0700

    Added helper for blockscore error

--- src/main/java/com/blockscore/models/error/BlockscoreError.java
+++ src/main/java/com/blockscore/models/error/BlockscoreError.java
@@ -2,6 +2,8 @@ package com.blockscore.models.error;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import retrofit.RetrofitError;
 
 /**
  * Blockscore Error model.
@@ -11,6 +13,21 @@ public class BlockscoreError {
     @NotNull
     @JsonProperty("error")
     private RequestError mRequestError;
+
+    /**
+     * Converts a Retrofit Error into a Blockscore Error.
+     * @param cause Retrofit error to convert.
+     * @return Blockscore Error
+     */
+    @Nullable
+    public static BlockscoreError getBlockscoreError(@NotNull final RetrofitError cause) {
+        Object rawError = cause.getBodyAs(BlockscoreError.class);
+        if (rawError instanceof BlockscoreError) {
+            return (BlockscoreError) rawError;
+        } else {
+            return null;
+        }
+    }
 
     /**
      * Returns the error details.
